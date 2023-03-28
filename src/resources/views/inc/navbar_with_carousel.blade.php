3f9310ed5789f93fcc7b0621ba617b1e77593280
Farhad-Hossain <farhad.cse@yahoo.com>
2023-03-29 00:23:33 +0600

    Home page 2

--- src/resources/views/inc/navbar_with_carousel.blade.php
+++ src/resources/views/inc/navbar_with_carousel.blade.php
@@ -1,17 +1,19 @@
 <div class="container-fluid position-relative p-0">
     @include('inc.nav')
     <div id="header-carousel" class="carousel slide carousel-fade" data-bs-ride="carousel">
-        <div class="carousel-inner">
+        <div class="carousel-inner px-4" style="height: 500px;">
             @if ($sliders)
             @foreach($sliders as $slider)
-            <div class="carousel-item {{$loop->index==0 ? 'active' : ''}}">
-                <img class="w-100" src="{{asset($slider->image_url)}}" style="max-height: 750px;" alt="Image">
+            <div class="carousel-item px-4 mx-2 {{$loop->index==0 ? 'active' : ''}}" style="border-radius: 4px;">
+                <img class="w-100" src="{{asset($slider->image_url)}}" style="max-height: 100%; border-radius: 2px;" alt="Image">
                 <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
                     <div class="p-3" style="max-width: 900px;">
                         <h5 class="text-white text-uppercase mb-3 animated slideInDown">{{$slider->secondary_title ?? ''}}</h5>
                         <h3 class="display-4 text-white mb-md-4 animated zoomIn">{{$slider->primary_title ?? ''}}</h3>
+                        {{--
                         <a href="{{route('quote')}}" class="btn btn-primary py-md-3 px-md-5 me-3 animated slideInLeft">Free Quote</a>
                         <a href="{{route('contact')}}" class="btn btn-outline-light py-md-3 px-md-5 animated slideInRight">Contact Us</a>
+                        --}}
                     </div>
                 </div>
             </div>        
