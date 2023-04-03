e8df33c4a43a1980e6eafbe07725ec3cb8ab8d9b
Farhad-Hossain <farhad.cse@yahoo.com>
2023-04-03 16:26:33 +0000

    Remove extra padding from Team, Quote and About section

--- src/resources/views/inc/about.blade.php
+++ src/resources/views/inc/about.blade.php
@@ -1,6 +1,6 @@
 <!-- About Start -->
 
-<div class="container-fluid py-5 wow fadeInUp" data-wow-delay="0.1s">
+<div class="container-fluid wow fadeInUp" data-wow-delay="0.1s">
         <div class="container py-5">
             <div class="row g-5">
                 <div class="col-lg-7">
--- src/resources/views/inc/team.blade.php
+++ src/resources/views/inc/team.blade.php
@@ -1,9 +1,9 @@
-<!-- Team Start -->
-<div class="container-fluid py-5 wow fadeInUp" data-wow-delay="0.1s">
+<!-- Start::Team -->
+<div class="container-fluid wow fadeInUp" data-wow-delay="0.1s">
     <div class="container py-5">
         <div class="section-title text-center position-relative pb-3 mb-5 mx-auto" style="max-width: 600px;">
             <h5 class="fw-bold text-primary text-uppercase">Team Members</h5>
-            <h1 class="mb-0">Professional Trainers Ready to Help You </h1>
+            <h1 class="mb-0">Our Professional Trainers </h1>
         </div>
         <div class="row g-5">
             @foreach($stuffs as $stuff)
@@ -28,4 +28,4 @@
         </div>
     </div>
 </div>
-    <!-- Team End -->
+<!-- End::Team  -->
