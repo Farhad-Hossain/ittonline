b87dd9e7b3c7432332772e16b1006ec1a8bdbb28
Farhad-Hossain <farhad.cse@yahoo.com>
2023-04-03 18:27:15 +0000

    Remove extra space from testimonial and why choose us section

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -48,7 +48,7 @@
     <!-- Facts Start -->
     @include('inc.about')
     <!-- Features Start -->
-    <div class="container-fluid py-5 wow fadeInUp" data-wow-delay="0.1s">
+    <div class="container-fluid wow fadeInUp" data-wow-delay="0.1s">
         <div class="container py-5">
             <div class="section-title text-center position-relative pb-3 mb-5 mx-auto" style="max-width: 600px;">
                 <h5 class="fw-bold text-primary text-uppercase">Why Choose Us</h5>
