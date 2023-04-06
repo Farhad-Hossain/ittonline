1e1d7b36ace88c8bee2efd7cc73e69b12b4690cf
Farhad-Hossain <farhad.cse@yahoo.com>
2023-04-06 18:29:49 +0000

    red_theme initiate

--- src/resources/views/inc/navbar.blade.php
+++ src/resources/views/inc/navbar.blade.php
@@ -1,8 +1,8 @@
 <!-- Navbar Start -->
 <div class="container-fluid position-relative p-0">
     @include('inc.nav')
-    <div class="container-fluid bg-primary py-5 bg-header" style="margin-bottom: 60px;">
-        <div class="row pt-5 pb-2">
+    <div class="container-fluid bg-primary py-5 bg-header" style="margin-bottom: 50px;">
+        <div class="row pb-2">
             <div class="col-12 pt-lg-2 mt-lg-2 text-center">
                 <h4 class="display-6 text-white animated zoomIn">{{$title}}</h4>
                 {{--
