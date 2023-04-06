31895ec56ac9a5948e28bc0607200c90f7bb90bf
Farhad-Hossain <farhad.cse@yahoo.com>
2023-04-06 16:24:25 +0000

    carousel padding fixed

--- src/resources/views/inc/navbar_with_carousel.blade.php
+++ src/resources/views/inc/navbar_with_carousel.blade.php
@@ -4,23 +4,22 @@
         <div class="container py-3">
         <div class="carousel-inner">
             @if ($sliders)
-            @foreach($sliders as $slider)
-            <div class="carousel-item {{$loop->index==0 ? 'active' : ''}}">
-                <img class="w-100" src="{{asset($slider->image_url)}}" style="max-height: 480px; border-radius: 2px;" alt="Image">
-                <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
-                    
-                    <div class="p-3" style="max-width: 100%;">
+                @foreach($sliders as $slider)
+                <div class="carousel-item {{$loop->index==0 ? 'active' : ''}}">
+                    <img class="w-100" src="{{asset($slider->image_url)}}" style="max-height: 480px; border-radius: 2px;" alt="Image">
+                    <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
                         
-                        <h5 class="text-white text-uppercase mb-3 animated slideInDown">{{$slider->secondary_title ?? ''}}</h5>
-                        <h3 class="display-4 text-white mb-md-4 animated zoomIn">{{$slider->primary_title ?? ''}}</h3>
-                        {{--
-                        <a href="{{route('quote')}}" class="btn btn-primary py-md-3 px-md-5 me-3 animated slideInLeft">Free Quote</a>
-                        <a href="{{route('contact')}}" class="btn btn-outline-light py-md-3 px-md-5 animated slideInRight">Contact Us</a>
-                        --}}
+                        <div class="p-3" style="max-width: 100%;">
+                            <h5 class="text-white text-uppercase mb-3 animated slideInDown">{{$slider->secondary_title ?? ''}}</h5>
+                            <h3 class="display-4 text-white mb-md-4 animated zoomIn">{{$slider->primary_title ?? ''}}</h3>
+                            {{--
+                            <a href="{{route('quote')}}" class="btn btn-primary py-md-3 px-md-5 me-3 animated slideInLeft">Free Quote</a>
+                            <a href="{{route('contact')}}" class="btn btn-outline-light py-md-3 px-md-5 animated slideInRight">Contact Us</a>
+                            --}}
+                        </div>
                     </div>
-                </div>
-            </div>        
-            @endforeach
+                </div>        
+                @endforeach
             @else
             <div class="carousel-item {{'active'}}">
                 <img class="w-100" src="https://picsum.photos/200/300.jpg" style="max-height: 480px;" alt="Image">
