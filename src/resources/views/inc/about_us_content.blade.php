c5ca20eaac415a04d068bd2a4d8e53956fdd2524
Farhad-Hossain <farhad.cse@yahoo.com>
2023-04-07 19:01:09 +0000

    Contact Us modified

--- src/resources/views/inc/about_us_content.blade.php
+++ src/resources/views/inc/about_us_content.blade.php
@@ -3,9 +3,6 @@
     <div class="container py-5">
         <div class="row g-5">
             <div class="col-12">
-                <div class="section-title position-relative pb-3 mb-5">
-                    <h5 class="fw-bold text-primary text-uppercase">{{$aboutUsMenu ? $aboutUsMenu->menu_name : ''}}</h5>
-                </div>
                 <p class="mb-4">
                     {!! $aboutUsMenu ? $aboutUsMenu->short_description : '' !!}
                 </p>
