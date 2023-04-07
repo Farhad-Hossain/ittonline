17ec5fdf7227d9bcf58b40aa0d5b0871cc934608
Farhad-Hossain <farhad.cse@yahoo.com>
2023-04-07 19:00:09 +0000

    Contact Us modified

--- src/resources/views/pages/about_us_content.blade.php
+++ src/resources/views/pages/about_us_content.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.master',['title'=>$aboutUsMenu->menu_name, 'nav_title'=>$aboutUsMenu->menu_name])
 @section('contents')
     @include('inc.about_us_content')
-    @include('inc.contact')
+    @include('inc.quote')
 @endsection
